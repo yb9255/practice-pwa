7797b69cbd112d9d2b4a939459ecd5832b5db14c
yb9255 <yoobin9255@gmail.com>
2023-02-20 22:41:32 +0900

    <fix> fix promise to async

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,23 +1,25 @@
-var deferredPrompt;
+let deferredPrompt;
 
 if (!window.Promise) {
   window.Promise = Promise;
 }
 
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("/sw.js")
-    .then(function () {
+  async function registorServiceWorker() {
+    try {
+      await navigator.serviceWorker.register("/sw.js");
       console.log("Service worker registered!");
-    })
-    .catch(function (err) {
+    } catch (error) {
       console.log(err);
-    });
+    }
+  }
 }
 
 window.addEventListener("beforeinstallprompt", function (event) {
   console.log("beforeinstallprompt fired");
   event.preventDefault();
+
   deferredPrompt = event;
+
   return false;
 });
